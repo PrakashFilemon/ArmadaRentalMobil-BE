18ab93aed5c763af78b0ffe244d4907625473292
PrakashFilemon <prakashfilemon@gmail.com>
2025-08-22 13:48:19 +0700

    Change Database.js-2

--- database.js
+++ database.js
@@ -2,26 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.PGUSER,
-    password: decodeURIComponent(process.env.PGPASSWORD),
-    database: process.env.PGDATABASE,
-    host: process.env.PGHOST,
-    port: process.env.PGPORT || 5432,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // wajib untuk Neon
-      },
-    },
-    logging: false,
-  },
-  test: {
-    username: process.env.PGUSER,
-    password: decodeURIComponent(process.env.PGPASSWORD),
-    database: process.env.PGDATABASE,
-    host: process.env.PGHOST,
-    port: process.env.PGPORT || 5432,
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     dialectOptions: {
       ssl: {
@@ -32,11 +13,7 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: process.env.PGUSER,
-    password: decodeURIComponent(process.env.PGPASSWORD),
-    database: process.env.PGDATABASE,
-    host: process.env.PGHOST,
-    port: process.env.PGPORT || 5432,
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     dialectOptions: {
       ssl: {
